d99c3594b535b1af0211ea0cc0d8423ad19a84fa
Ubuntu <ubuntu@ip-172-31-53-179.ec2.internal>
2022-04-06 23:05:01 +0000

    Updated

--- num_guess_try_catch.py
+++ num_guess_try_catch.py
@@ -10,26 +10,25 @@ def main():
     # a number between 1 and 9
     # print hidden
 
-    # input
-    guess = int(input("Guess a number between 1 and 9 :"))
-    print("")
-    
-    try: 
-
-      # check the numbers is equal to hidden
+    # Compare the randome number to the guess and display the output
+    try:
+        guess = int(input("Guess a number between 1 and 9 :"))
+        integer_as_number = int(guess)
+    # check the numbers is equal to hidden
         if guess == hidden:
-            integer_as_number = int(guess)
+
             print("you guessed right")
 
-# check the numbers is not equal to hidden
+    # check the numbers is not equal to hidden
         else:
             print("you guessed wrong")
             print("The correct answer is")
-            print(hidden) 
-    except Exception: 
-        print("This was not an integer")
+            print(hidden)
+    except Exception:
+        print("This was not an integer, please enter an integer")
     finally:
         print("Thanks for playing")
+
 
 if __name__ == "__main__":
     main()
